772a9972bfca64f0d4e3ca1d1ddf7c1c245b3c41
Canaan φ(゜▽゜*)♪ <reakak1@gmail.com>
2021-06-22 16:16:55 +0300

    client streaming example done

--- testinGRPC/Services/Weather.cs
+++ testinGRPC/Services/Weather.cs
@@ -1,6 +1,9 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf.WellKnownTypes;
+
+using Grpc.Core;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 using System;
 using System.Collections.Generic;
@@ -17,25 +20,95 @@ namespace testinGRPC.Services
   public class Weather : WeatherService.WeatherServiceBase
   {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<Weather> _logger;
     private readonly IConfiguration _config;
-    public Weather(IHttpClientFactory httpClientFactory, IConfiguration config)
+    private readonly string API_KEY;
+    public Weather(IHttpClientFactory httpClientFactory, IConfiguration config, ILogger<Weather> logger)
     {
+      _logger = logger;
       _config = config;
       _httpClientFactory = httpClientFactory;
+      API_KEY = _config["_OpenWeatherAPI"];
+
     }
 
     public override async Task<WeatherResponse> GetCurrentWeather(GetCurrentWeatherRequest request, ServerCallContext context)
     {
-      var API_KEY = _config["_OpenWeatherAPI"];
       var httpClient = _httpClientFactory.CreateClient();
-      var responseText =await httpClient.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={request.City}&appid={API_KEY}&units={request.Units}");
+      Temprature temperatures = await GetCurrentWeeatherAsunc(request, API_KEY, httpClient);
+
+      return new WeatherResponse 
+      {
+        Temperature = temperatures!.Main.Temp,
+        FeelsLike = temperatures.Main.FeelsLike,
+        Time = Timestamp.FromDateTime(DateTime.UtcNow),
+        City = request.City
+      };
+    }
+
+    public override async Task GetCurrentWeatherStream(GetCurrentWeatherRequest request, IServerStreamWriter<WeatherResponse> responseStream, ServerCallContext context)
+    {
+      var httpClient = _httpClientFactory.CreateClient();
+      for (var i = 0; i < 30; i++)
+      {
+        if (context.CancellationToken.IsCancellationRequested)
+        {
+          _logger.LogInformation("Canceled by the user");
+          break;
+        }
+        Temprature temperatures = await GetCurrentWeeatherAsunc(request, API_KEY, httpClient);
+
+        await responseStream.WriteAsync(
+          new WeatherResponse
+          {
+            Temperature = temperatures!.Main.Temp,
+            FeelsLike = temperatures.Main.FeelsLike,
+            Time = Timestamp.FromDateTime(DateTime.UtcNow),
+            City = request.City
+          }) ;
+            await Task.Delay(1000);
+      }
+ 
+    }
+
+
+
+    public override async Task<MultiWeatherResponse> GetMultiCurrentWeatherStream(IAsyncStreamReader<GetCurrentWeatherForCityRequest> requestStream, ServerCallContext context)
+    {
+      var httpClient = _httpClientFactory.CreateClient();
+      var response = new MultiWeatherResponse
+      {
+        Weather = { }
+      };
+      await foreach (var request in requestStream.ReadAllAsync())
+      {
+        var temperatures = await GetCurrentWeeatherAsunc(request, API_KEY, httpClient);
+        response.Weather.Add(
+          new WeatherResponse
+          {
+            Temperature = temperatures!.Main.Temp,
+            FeelsLike = temperatures.Main.FeelsLike,
+            Time = Timestamp.FromDateTime(DateTime.UtcNow),
+            City = request.City
+          });
+      }
+      return response;
+    }
+
+    private async Task<Temprature> GetCurrentWeeatherAsunc(GetCurrentWeatherForCityRequest request, string aPI_KEY, HttpClient httpClient)
+    {
+      var responseText = await httpClient.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={request.City}&appid={API_KEY}&units={request.Units}");
 
       var temperatures = JsonSerializer.Deserialize<Temprature>(responseText);
+      return temperatures;
+    }
 
-      return new WeatherResponse{
-        Temperature = temperatures!.Main.Temp,
-        FeelsLike = temperatures.Main.FeelsLike
-      };
+    private static async Task<Temprature> GetCurrentWeeatherAsunc(GetCurrentWeatherRequest request, string API_KEY, HttpClient httpClient)
+    {
+      var responseText = await httpClient.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={request.City}&appid={API_KEY}&units={request.Units}");
+
+      var temperatures = JsonSerializer.Deserialize<Temprature>(responseText);
+      return temperatures;
     }
   }
 }
